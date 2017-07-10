0b2f535639bab2a628a38912923da00e86ceb9a2
Dennis Roberts <dennis@cyverse.org>
2017-07-10 10:02:49 -0700

    removed the Clojure event service; the default event service implementation should work fine for our purposes

--- clojure-event-service/src/org/cyverse/dataone/ClojureEventServiceFactory.clj
+++
@@ -1,6 +0,0 @@
-(ns org.cyverse.dataone.ClojureEventServiceFactory
-  (:refer-clojure)
-  (:gen-class :extends org.irods.jargon.dataone.events.AbstractDataOneEventServiceFactory))
-
-(defn -instance [this publication-context irods-account]
-  (org.cyverse.dataone.ClojureEventService. irods-account publication-context))
