aa94aaeb183114957f6e2bbadcdd4572f9aab791
Dennis Roberts <dennis@cyverse.org>
2017-09-18 15:41:43 -0700

    CORE-8925: updated the repo service plugin to work with data objects once again

--- clojure-repo-service/src/clojure/org/cyverse/dataone/ClojureRepoService.clj
+++ clojure-repo-service/src/clojure/org/cyverse/dataone/ClojureRepoService.clj
@@ -3,9 +3,9 @@
   (:import [org.dataone.service.types.v1 Identifier]
            [org.irods.jargon.core.exception FileNotFoundException]
            [org.irods.jargon.core.pub DataAOHelper]
-           [org.irods.jargon.core.query CollectionAndDataObjectListingEntry$ObjectType IRODSGenQueryBuilder
-            QueryConditionOperators]
-           [org.irods.jargon.dataone.model CollectionDataOneObject DataOneObjectListResponse]
+           [org.irods.jargon.core.query CollectionAndDataObjectListingEntry$ObjectType GenQueryOrderByField$OrderByType
+            IRODSGenQueryBuilder QueryConditionOperators RodsGenQueryEnum]
+           [org.irods.jargon.dataone.model DataOneObjectListResponse FileDataOneObject]
            [java.util Date])
   (:require [clojure.string :as string]
             [clojure.tools.logging :as log])
@@ -22,6 +22,8 @@
 (def ^:private default-root "/iplant/home/shared/commons_repo/curated")
 (def ^:private default-page-length "50")
 (def ^:private default-format "application/octet-stream")
+(def ^:private default-offset 0)
+(def ^:private default-limit 500)
 
 ;; Functions to retrieve configuration settings.
 
@@ -61,32 +63,74 @@
   (.getIRODSGenQueryExecutor (.. this getPublicationContext getIrodsAccessObjectFactory)
                              (.getIrodsAccount this)))
 
+(defn- lazy-rs
+  "Converts a query result set into a lazy sequence of query result sets."
+  [executor rs]
+  (if (.isHasMoreRecords rs)
+    (lazy-seq (cons rs (lazy-rs executor (.getMoreResults executor rs))))
+    (do (.closeResults executor rs) [rs])))
+
+(defn- lazy-gen-query
+  "Performs a general query and returns a lazy sequence of results."
+  [this offset query]
+  (let [executor (get-gen-query-executor this)]
+    (mapcat (fn [rs] (.getResults rs))
+            (lazy-rs executor (.executeIRODSQuery executor query (or offset default-offset))))))
+
+(defn- gen-query
+  "Performs a general query and returns the result set. This result set will be closed automatically."
+  [this query offset]
+  (.executeIRODSQueryAndCloseResult (get-gen-query-executor this) query offset))
+
+(defn- add-modify-time-condition [builder operator date]
+  (.addConditionAsGenQueryField builder RodsGenQueryEnum/COL_D_MODIFY_TIME operator (quot (.getTime date) 1000)))
+
+(defn- add-coll-name-condition [builder operator value]
+  (.addConditionAsGenQueryField builder RodsGenQueryEnum/COL_COLL_NAME operator value))
+
+(defn- add-attribute-name-condition [builder operator value]
+  (.addConditionAsGenQueryField builder RodsGenQueryEnum/COL_META_DATA_ATTR_NAME operator value))
+
 ;; Functions to retrieve the list of exposed identifiers.
 
-(defn- list-matching-identifiers [this from-date to-date])
+(defn- build-id-query [this]
+  (-> (IRODSGenQueryBuilder. true nil)
+      (.addSelectAsGenQueryValue RodsGenQueryEnum/COL_META_DATA_ATTR_VALUE)
+      (.addOrderByGenQueryField RodsGenQueryEnum/COL_META_DATA_ATTR_VALUE GenQueryOrderByField$OrderByType/ASC)
+      (add-coll-name-condition QueryConditionOperators/LIKE (str (get-root this) "%"))
+      (add-attribute-name-condition QueryConditionOperators/EQUAL (get-uuid-attr this))
+      (.exportIRODSQueryFromBuilder (get-query-page-length this))))
+
+(defn- list-exposed-identifiers [this]
+  (mapv (fn [row] (doto (Identifier.) (.setValue (.getColumn row 0))))
+        (lazy-gen-query this (build-id-query this))))
 
 ;; Functions to retrieve the list of exposed data objects.
 
-(defn- add-modify-time-condition [builder operator date]
-  (.addConditionAsGenQueryField builder "DATA_MODIFY_TIME" operator (quot (.getTime date) 1000)))
-
-(defn- add-coll-name-condition [builder operator value]
-  (.addConditionAsGenQueryField builder "COLL_NAME" operator value))
-
-(defn- build-data-object-listing-query [this from-date to-date]
+(defn- build-data-object-listing-query [this from-date to-date limit]
   (-> (IRODSGenQueryBuilder. true false nil)
+      (.addSelectAsGenQueryValue RodsGenQueryEnum/COL_META_DATA_ATTR_NAME)
+      (.addSelectAsGenQueryValue RodsGenQueryEnum/COL_META_DATA_ATTR_VALUE)
+      (.addSelectAsGenQueryValue RodsGenQueryEnum/COL_META_DATA_ATTR_UNITS)
       (DataAOHelper/addDataObjectSelectsToBuilder)
       (add-modify-time-condition QueryConditionOperators/NUMERIC_GREATER_THAN_OR_EQUAL_TO from-date)
       (add-modify-time-condition QueryConditionOperators/NUMERIC_LESS_THAN_OR_EQUAL_TO to-date)
-      (add-coll-name-condition QueryConditionOperators/LIKE (str (get-root this) "%"))))
+      (add-coll-name-condition QueryConditionOperators/LIKE (str (get-root this) "%"))
+      (add-attribute-name-condition QueryConditionOperators/EQUAL (get-uuid-attr this))
+      (.addOrderByGenQueryField RodsGenQueryEnum/COL_D_DATA_PATH GenQueryOrderByField$OrderByType/ASC)
+      (.exportIRODSQueryFromBuilder limit)))
 
-;; TODO: finish implementing this function. We need to get the result set and verify that we can extract all of the
-;; results in the case where the limit is greater than the query length or not defined at all. This could cause a
-;; problem or slowness when we're closing the result set between pages.
-(defn- list-exposed-data-objects [this from-date to-date start-index limit]
-  (->> (build-data-object-listing-query this from-date to-date)
-       (.exportIRODSQueryFromBuilder (get-query-page-length))
-       (.executeIRODSQueryAndCloseResult (or start-index 0))))
+(defn- file-data-one-object-from-row [this row]
+  (FileDataOneObject.
+   (.getPublicationContext this)
+   (.getIrodsAccount this)
+   (.getColumn row RodsGenQueryEnum/COL_META_DATA_ATTR_VALUE)
+   (DataAOHelper/buildDomainFromResultSetRow row)))
+
+(defn- list-exposed-data-objects [this from-date to-date format-id start-index count]
+  (let [rs       (gen-query this (build-data-object-listing-query this from-date to-date format-id count))
+        elements (mapv (partial file-data-one-object-from-row this) (.getResults rs))]
+    (DataOneObjectListResponse. elements (.getTotalRecords rs) start-index)))
 
 ;; Last modification date functions.
 
@@ -104,17 +148,18 @@
   [[irods-account publication-context] {}])
 
 (defn -getListOfDataoneExposedIdentifiers [this]
-  (mapv (fn [s] (doto (Identifier.) (.setValue (:value s))))
-        (list-exposed-identifiers this)))
+  (list-exposed-identifiers this))
 
-(defn -getExposedObjects [this from-date to-date format-id _ start-index limit]
-  (if (or (nil? (some-> format-id .getValue)) (= (.getValue format-id) default-format))
-    (try
-      (log/spy :warn (list-exposed-data-objects this from-date to-date start-index limit))
-      (catch Throwable t
-        (log/error t)
-        (throw t)))
-    (DataOneObjectListResponse. [] 0 (or start-index 0))))
+(defn -getExposedObjects [this from-date to-date format-id _ offset limit]
+  (let [offset (or offset default-offset)
+        limit       (or limit default-limit)]
+    (if (or (nil? (some-> format-id .getValue)) (= (.getValue format-id) default-format))
+      (try
+        (log/spy :warn (list-exposed-data-objects this from-date to-date offset limit))
+        (catch Throwable t
+          (log/error t)
+          (throw t)))
+      (DataOneObjectListResponse. [] offset limit))))
 
 (defn -getLastModifiedDate [this path]
   (get-last-modified-date this path))
