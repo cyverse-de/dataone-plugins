7787f76f52cacfc2c9b5745b212e7097661f5afd
Dennis Roberts <dennis@cyverse.org>
2017-09-19 17:10:25 -0700

    CORE-8925: updated a few fields in the project.clj files

--- pid-service/project.clj
+++ pid-service/project.clj
@@ -1,8 +1,7 @@
 (defproject org.cyverse/dataone-pid-service "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Plugin for the DataONE member node service."
+  :license {:name "BSD"
+            :url "http://www.cyverse.org/sites/default/files/iPLANT-LICENSE.txt"}
   :uberjar-name "dataone-pid-service-standalone.jar"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.logging "0.3.1"]
